ed1dffdd8e0e388e53f8dfc6a92c12bfc2bca7e4
Juwuyi <ju.51@qq.com>
2016-03-04 01:15:06 +0800

    模板文件修改
    删除不用的

--- roles/www_example_com.py
+++ roles/www_example_com.py
@@ -1,52 +1,65 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 03 15:20:11 2016
 
-@author: Lab
 """
-
+@author: ??????
+"""
 
 import requests
 import bs4
 import sys
-sys.path.append('..')
-import Config
+import os
+if os.path.abspath('../modules') not in sys.path:
+    sys.path.append(os.path.abspath('../modules'))
+Const = __import__('Const')
+
+
+
 
 # ******************************************常量
-
-latest_url = 'http://www.zdfans.com/page/1'     
-allowed_domain = 'www.zdfans.com'
-time_out = 3
-            
-
+latest_url = '??????'  
 # ******************************************函数
-
-
 def page_parser(html):
     soup = bs4.BeautifulSoup(html)
     try:
-        next_page_link = soup.find(lambda node: u'下一页' == node.string)['href']
-    except Exception as ex:
-        print 'warning at',allowed_domain,'finished or aborted'
-        print ex.message
+        next_page_link = ???
+    except:
         next_page_link = None
-    metas = soup.find(name='ul',class_='excerpt')
-    infos = []
-    for li in metas.children:
-        info = ConfigRPG.article_Tplt.copy()
-        info['article_link'] = li.a['href']
-        info['article_title'] = li.h2.a.string
-        info['content'] = li.find(name='div',class_='note').string
-        info['pic_link'] = li.a.img['src']
-        info['website'] = allowed_domain
-        infos.append(info)
-    return next_page_link,infos
+    
+    ret_dic_list = []
+    metas = ???
+    for meta in metas.children:
+        info = Const.ret_dic_tplt.copy()
+        info['article_title'] = ???
+        info['article_link'] = ???
+        info['article_pic_link'] = ???
+        info['article_website'] = 'www.???.com'
+        info['article_note'] = ???
+        
+        info['article_post_date'],\
+        info['article_cnt_up'],\
+        info['article_tags'],\
+        info['article_download_links'],\
+        info['article_content'] = subpage_parser(requests.get(info['article_link']).text)
+        ret_dic_list.append(info)
+    #返回格式不改变 下一页链接+本页内容信息对应的词典列表
+    return next_page_link,ret_dic_list
 
+def subpage_parser(html):
+    # 需要进去子页面 返回日期、正文内容等等
+    soup = bs4.BeautifulSoup(html)
+    article_post_date = soup.find(name='p',class_='meta-info').get_text().split()[0]    #.encode('utf-8').replace('-','')
+    article_cnt_up = int(soup.find(name='span',class_='view').span.string.encode('utf-8').replace(',',''))
+    article_tags = [tmp.string.encode('utf-8') for tmp in soup.find(name='p',class_='meta-info').find_all(name='a',rel='category tag')]
+    article_download_links = [] 
+    #以上自动抓去下载链接 以及其密码 求助攻
+    article_content = soup.find(name='div',class_='entry').prettify().encode('utf-8')   #unicode.encode('utf-8')
+    return article_post_date,article_cnt_up,article_tags,article_download_links,article_content
 
 
 if "__main__" == __name__:
-    html = requests.get("http://www.zdfans.com/page/33").text
-    next_page_link,infos = page_parser(html)
-    #nextPage,metas = find_linker_from_html(html)
+    html = requests.get("???").text
+    next_page_link,ret_dic_list = page_parser(html)
+    
+    
     
     
